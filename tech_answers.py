fe077a44af479c2986b9f214cdefd3226305c822
binshadpb <binshadbinu40@gmail.com>
2021-10-09 08:59:04 +0530

    question 2 answer

--- tech_answers.py
+++ tech_answers.py
@@ -17,5 +17,25 @@ print("Value of x : ", x, " and y : ", y)
 
 
 
+# 2. The user enters a string and a substring. You have to print the number of times that the substring occurs in the given string. String traversal will take place from left to right, not from right to left. String letters are case sensitive.
+
+
+def count_substring(string, sub_string):
+    count = 0
+    for i in range(len(string)):
+        if string[i:].startswith(sub_string):
+            count += 1
+    return count
+
+string = input("Enter the String").strip()
+sub_string = input("Enter the Substring").strip()
+
+count = count_substring(string, sub_string)
+print(count)
+
+
+
+
+
 
 
