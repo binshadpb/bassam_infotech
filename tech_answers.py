0651d068b74081040a49f38730913956594b30b8
binshadpb <binshadbinu40@gmail.com>
2021-10-09 09:01:47 +0530

    question 3 answer

--- tech_answers.py
+++ tech_answers.py
@@ -35,6 +35,16 @@ print(count)
 
 
 
+# 3. You are given a string and your task is to swap cases. In other words, convert all lowercase letters to uppercase letters and vice versa.
+
+
+def swap_case(s):
+    return s.swapcase()
+
+if __name__ == '__main__':
+    s = input("Enter the String")
+    result = swap_case(s)
+    print(result)
 
 
 
