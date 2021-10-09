7fc6f5c39828bbda23cbea2edfe2ba3176c41219
binshadpb <binshadbinu40@gmail.com>
2021-10-09 08:49:22 +0530

    question 1 answer

---
+++ tech_answers.py
@@ -0,0 +1,21 @@
+
+# 1. Consider two variables ‘var1’, ‘var2’ with integer values. Without the use of a third variable, swap the values of these two variables.
+
+x = 6
+y = 15
+
+print("Before swapping: ")
+print("Value of x : ", x, " and y : ", y)
+
+x = x + y
+y = x - y
+x = x - y
+
+print("After swapping: ")
+print("Value of x : ", x, " and y : ", y)
+
+
+
+
+
+
